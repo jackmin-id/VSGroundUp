c53e6ace1d0c11b430af0d84eef4ca81d6ec3b93
jackmin-id <minjwus@gmail.com>
2021-10-24 16:22:46 -0600

    page 201

---
+++ DailyRate/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            Console.Write("Please enter a positive integer: ");
+            string s = Console.ReadLine();
+            long factorialValue = CalculateFactor(s);
+            Console.WriteLine($"Factorial is {factorialValue}");
+        }
+
+        private long CalculateFactor(string s)
+        {
+            int inputValue = int.Parse(s);
+
+            return factorial(inputValue);
+        }
+
+        private int factorial(int v)
+        {
+            if (v == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return v * factorial(v - 1);
+            }
+        }
+    }
+}
